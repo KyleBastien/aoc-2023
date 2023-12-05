c38970b15230cb17459ce5807b07fe0d5a2d153b
Kyle-Bastien <Kyle.Bastien@docusign.com>
2023-12-04 23:18:50 -0800

    Day 5.

--- src/day05/part2.ts
+++ src/day05/part2.ts
@@ -1,5 +1,38 @@
 import { parseInput } from '../util';
+import { extractInfo } from './part1';
 
-const input = parseInput();
+const input = parseInput({ split: false });
 
-// TODO: Complete Part 2
+/**
+ * --- Part Two ---
+ * Everyone will starve if you only plant such a small number of seeds. Re-reading the almanac, it looks like the seeds: line actually describes ranges of seed numbers.
+ *
+ * The values on the initial seeds: line come in pairs. Within each pair, the first value is the start of the range and the second value is the length of the range. So, in the first line of the example above:
+ *
+ * seeds: 79 14 55 13
+ * This line describes two ranges of seed numbers to be planted in the garden. The first range starts with seed number 79 and contains 14 values: 79, 80, ..., 91, 92. The second range starts with seed number 55 and contains 13 values: 55, 56, ..., 66, 67.
+ *
+ * Now, rather than considering four seed numbers, you need to consider a total of 27 seed numbers.
+ *
+ * In the above example, the lowest location number can be obtained from seed number 82, which corresponds to soil 84, fertilizer 84, water 84, light 77, temperature 45, humidity 46, and location 46. So, the lowest location number is 46.
+ *
+ * Consider all of the initial seed numbers listed in the ranges on the first line of the almanac. What is the lowest location number that corresponds to any of the initial seed numbers?
+ */
+function part2(input: string): number {
+  const [seeds, data] = extractInfo(input);
+  let minLocation = Number.MAX_SAFE_INTEGER;
+
+  for (let i = 0; i < seeds.length; i += 2) {
+    const [start, length] = seeds.slice(i, i + 2);
+    for (let j = start; j < start + length; j++) {
+      const location = data.seedToLocation(j);
+      if (location < minLocation) {
+        minLocation = location;
+      }
+    }
+  }
+
+  return minLocation;
+}
+
+export default part2(input);
