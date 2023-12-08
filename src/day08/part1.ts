a98572ee44a617fcaf7f18d3bb6f3db9271d930f
Kyle-Bastien <Kyle.Bastien@docusign.com>
2023-12-07 21:34:17 -0800

    Day 8

--- src/day08/part1.ts
+++ src/day08/part1.ts
@@ -1,5 +1,69 @@
 import { parseInput } from '../util';
 
-const input = parseInput();
+const input = parseInput({ split: { mapper: false } });
 
-// TODO: Complete Part 1
+/**
+ * --- Day 8: Haunted Wasteland ---
+ * You're still riding a camel across Desert Island when you spot a sandstorm quickly approaching. When you turn to warn the Elf, she disappears before your eyes! To be fair, she had just finished warning you about ghosts a few minutes ago.
+ *
+ * One of the camel's pouches is labeled "maps" - sure enough, it's full of documents (your puzzle input) about how to navigate the desert. At least, you're pretty sure that's what they are; one of the documents contains a list of left/right instructions, and the rest of the documents seem to describe some kind of network of labeled nodes.
+ *
+ * It seems like you're meant to use the left/right instructions to navigate the network. Perhaps if you have the camel follow the same instructions, you can escape the haunted wasteland!
+ *
+ * After examining the maps for a bit, two nodes stick out: AAA and ZZZ. You feel like AAA is where you are now, and you have to follow the left/right instructions until you reach ZZZ.
+ *
+ * This format defines each node of the network individually. For example:
+ *
+ * RL
+ *
+ * AAA = (BBB, CCC)
+ * BBB = (DDD, EEE)
+ * CCC = (ZZZ, GGG)
+ * DDD = (DDD, DDD)
+ * EEE = (EEE, EEE)
+ * GGG = (GGG, GGG)
+ * ZZZ = (ZZZ, ZZZ)
+ * Starting with AAA, you need to look up the next element based on the next left/right instruction in your input. In this example, start with AAA and go right (R) by choosing the right element of AAA, CCC. Then, L means to choose the left element of CCC, ZZZ. By following the left/right instructions, you reach ZZZ in 2 steps.
+ *
+ * Of course, you might not find ZZZ right away. If you run out of left/right instructions, repeat the whole sequence of instructions as necessary: RL really means RLRLRLRLRLRLRLRL... and so on. For example, here is a situation that takes 6 steps to reach ZZZ:
+ *
+ * LLR
+ *
+ * AAA = (BBB, BBB)
+ * BBB = (AAA, ZZZ)
+ * ZZZ = (ZZZ, ZZZ)
+ * Starting at AAA, follow the left/right instructions. How many steps are required to reach ZZZ?
+ */
+interface Node {
+  left: string;
+  right: string;
+}
+
+function handleInput(lines: string[]): [string, Map<string, Node>] {
+  const instructions = lines[0];
+
+  const graph: Map<string, Node> = new Map();
+
+  for (const line of lines.slice(2)) {
+    const [node, left, right] = line.match(/[A-Z]{3}/g)!;
+    graph.set(node, { left, right });
+  }
+
+  return [instructions, graph];
+}
+
+function part1(input: string[]): number {
+  const [instructions, graph] = handleInput(input);
+
+  let curr = 'AAA';
+  for (let p1 = 1; ; p1++) {
+    const rl = instructions[(p1 - 1) % instructions.length];
+    curr = graph.get(curr)![rl === 'R' ? 'right' : 'left'];
+
+    if (curr === 'ZZZ') {
+      return p1;
+    }
+  }
+}
+
+export default part1(input);
--- src/day08/part2.ts
+++ src/day08/part2.ts
@@ -1,5 +1,98 @@
 import { parseInput } from '../util';
 
-const input = parseInput();
+const input = parseInput({ split: { mapper: false } });
 
-// TODO: Complete Part 2
+/**
+ * --- Part Two ---
+ * The sandstorm is upon you and you aren't any closer to escaping the wasteland. You had the camel follow the instructions, but you've barely left your starting position. It's going to take significantly more steps to escape!
+ *
+ * What if the map isn't for people - what if the map is for ghosts? Are ghosts even bound by the laws of spacetime? Only one way to find out.
+ *
+ * After examining the maps a bit longer, your attention is drawn to a curious fact: the number of nodes with names ending in A is equal to the number ending in Z! If you were a ghost, you'd probably just start at every node that ends with A and follow all of the paths at the same time until they all simultaneously end up at nodes that end with Z.
+ *
+ * For example:
+ *
+ * LR
+ *
+ * 11A = (11B, XXX)
+ * 11B = (XXX, 11Z)
+ * 11Z = (11B, XXX)
+ * 22A = (22B, XXX)
+ * 22B = (22C, 22C)
+ * 22C = (22Z, 22Z)
+ * 22Z = (22B, 22B)
+ * XXX = (XXX, XXX)
+ * Here, there are two starting nodes, 11A and 22A (because they both end with A). As you follow each left/right instruction, use that instruction to simultaneously navigate away from both nodes you're currently on. Repeat this process until all of the nodes you're currently on end with Z. (If only some of the nodes you're on end with Z, they act like any other node and you continue as normal.) In this example, you would proceed as follows:
+ *
+ * Step 0: You are at 11A and 22A.
+ * Step 1: You choose all of the left paths, leading you to 11B and 22B.
+ * Step 2: You choose all of the right paths, leading you to 11Z and 22C.
+ * Step 3: You choose all of the left paths, leading you to 11B and 22Z.
+ * Step 4: You choose all of the right paths, leading you to 11Z and 22B.
+ * Step 5: You choose all of the left paths, leading you to 11B and 22C.
+ * Step 6: You choose all of the right paths, leading you to 11Z and 22Z.
+ * So, in this example, you end up entirely on nodes that end in Z after 6 steps.
+ *
+ * Simultaneously start on every node that ends with A. How many steps does it take before you're only on nodes that end with Z?
+ */
+interface Node {
+  left: string;
+  right: string;
+}
+
+function handleInput(lines: string[]): [string, Map<string, Node>] {
+  const instructions = lines[0];
+
+  const graph: Map<string, Node> = new Map();
+
+  for (const line of lines.slice(2)) {
+    const [node, left, right] = line.match(/[A-Z]{3}/g)!;
+    graph.set(node, { left, right });
+  }
+
+  return [instructions, graph];
+}
+
+function part2(input: string[]): number {
+  const [instructions, graph] = handleInput(input);
+
+  const nodesEndingA: string[] = [];
+  const cycles: number[] = [];
+
+  for (const node of graph.keys()) {
+    if (node.endsWith('A')) {
+      nodesEndingA.push(node);
+      cycles.push(0);
+    }
+  }
+
+  const currs = [...nodesEndingA];
+
+  for (let step = 1; ; step++) {
+    for (let c = 0; c < currs.length; c++) {
+      currs[c] = graph.get(currs[c])![
+        instructions[(step - 1) % instructions.length] === 'R'
+          ? 'right'
+          : 'left'
+      ];
+      if (currs[c].endsWith('Z')) {
+        cycles[c] = step;
+      }
+    }
+
+    if (cycles.every(cy => cy !== 0)) {
+      // Calculate the least common multiple (LCM) of cycles
+      return cycles.reduce((a, b) => (a * b) / gcd(a, b));
+    }
+  }
+}
+
+// Helper function to calculate greatest common divisor (GCD)
+function gcd(a: number, b: number): number {
+  if (b === 0) {
+    return a;
+  }
+  return gcd(b, a % b);
+}
+
+export default part2(input);
