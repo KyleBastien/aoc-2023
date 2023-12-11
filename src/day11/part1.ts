0a0aaf102dec10b9a88331de80cda5a3b5f70bd9
Kyle-Bastien <Kyle.Bastien@docusign.com>
2023-12-10 21:25:42 -0800

    Day 11.

--- src/day11/part1.ts
+++ src/day11/part1.ts
@@ -1,5 +1,161 @@
 import { parseInput } from '../util';
 
-const input = parseInput();
+const input = parseInput({ split: { mapper: false } });
 
-// TODO: Complete Part 1
+/**
+ * --- Day 11: Cosmic Expansion ---
+ * You continue following signs for "Hot Springs" and eventually come across an observatory. The Elf within turns out to be a researcher studying cosmic expansion using the giant telescope here.
+ *
+ * He doesn't know anything about the missing machine parts; he's only visiting for this research project. However, he confirms that the hot springs are the next-closest area likely to have people; he'll even take you straight there once he's done with today's observation analysis.
+ *
+ * Maybe you can help him with the analysis to speed things up?
+ *
+ * The researcher has collected a bunch of data and compiled the data into a single giant image (your puzzle input). The image includes empty space (.) and galaxies (#). For example:
+ *
+ * ...#......
+ * .......#..
+ * #.........
+ * ..........
+ * ......#...
+ * .#........
+ * .........#
+ * ..........
+ * .......#..
+ * #...#.....
+ * The researcher is trying to figure out the sum of the lengths of the shortest path between every pair of galaxies. However, there's a catch: the universe expanded in the time it took the light from those galaxies to reach the observatory.
+ *
+ * Due to something involving gravitational effects, only some space expands. In fact, the result is that any rows or columns that contain no galaxies should all actually be twice as big.
+ *
+ * In the above example, three columns and two rows contain no galaxies:
+ *
+ *    v  v  v
+ *  ...#......
+ *  .......#..
+ *  #.........
+ * >..........<
+ *  ......#...
+ *  .#........
+ *  .........#
+ * >..........<
+ *  .......#..
+ *  #...#.....
+ *    ^  ^  ^
+ * These rows and columns need to be twice as big; the result of cosmic expansion therefore looks like this:
+ *
+ * ....#........
+ * .........#...
+ * #............
+ * .............
+ * .............
+ * ........#....
+ * .#...........
+ * ............#
+ * .............
+ * .............
+ * .........#...
+ * #....#.......
+ * Equipped with this expanded universe, the shortest path between every pair of galaxies can be found. It can help to assign every galaxy a unique number:
+ *
+ * ....1........
+ * .........2...
+ * 3............
+ * .............
+ * .............
+ * ........4....
+ * .5...........
+ * ............6
+ * .............
+ * .............
+ * .........7...
+ * 8....9.......
+ * In these 9 galaxies, there are 36 pairs. Only count each pair once; order within the pair doesn't matter. For each pair, find any shortest path between the two galaxies using only steps that move up, down, left, or right exactly one . or # at a time. (The shortest path between two galaxies is allowed to pass through another galaxy.)
+ *
+ * For example, here is one of the shortest paths between galaxies 5 and 9:
+ *
+ * ....1........
+ * .........2...
+ * 3............
+ * .............
+ * .............
+ * ........4....
+ * .5...........
+ * .##.........6
+ * ..##.........
+ * ...##........
+ * ....##...7...
+ * 8....9.......
+ * This path has length 9 because it takes a minimum of nine steps to get from galaxy 5 to galaxy 9 (the eight locations marked # plus the step onto galaxy 9 itself). Here are some other example shortest path lengths:
+ *
+ * Between galaxy 1 and galaxy 7: 15
+ * Between galaxy 3 and galaxy 6: 17
+ * Between galaxy 8 and galaxy 9: 5
+ * In this example, after expanding the universe, the sum of the shortest path between all 36 pairs of galaxies is 374.
+ *
+ * Expand the universe, then find the length of the shortest path between every pair of galaxies. What is the sum of these lengths?
+ */
+// Function to find shortest path between galaxies
+export function shortestPathOnGrid(start: number[], end: number[]): number {
+  const [x1, y1] = start;
+  const [x2, y2] = end;
+
+  return Math.abs(x1 - x2) + Math.abs(y1 - y2);
+}
+
+function part1(input: string[]): number {
+  const grid = input.map(line => line.split(''));
+
+  const newRows: string[][] = [];
+  for (const row of grid) {
+    if (row.every(seat => seat === '.')) {
+      newRows.push(row);
+      newRows.push(row); // add extra
+    } else {
+      newRows.push(row);
+    }
+  }
+
+  const newGrid: string[][] = new Array(newRows.length).fill(0).map(() => []);
+
+  for (let i = 0; i < newRows[0].length; i++) {
+    let allDots = true;
+    for (let j = 0; j < newRows.length; j++) {
+      if (newRows[j][i] !== '.') {
+        allDots = false;
+        break;
+      }
+    }
+
+    if (allDots) {
+      for (let j = 0; j < newRows.length; j++) {
+        newGrid[j].push('.');
+        newGrid[j].push('.');
+      }
+    } else {
+      for (let j = 0; j < newRows.length; j++) {
+        newGrid[j].push(newRows[j][i]);
+      }
+    }
+  }
+
+  const galaxies: number[][] = [];
+
+  for (let i = 0; i < newGrid[0].length; i++) {
+    for (let j = 0; j < newGrid.length; j++) {
+      if (newGrid[j][i] === '#') {
+        galaxies.push([i, j]);
+      }
+    }
+  }
+
+  let pathSum = 0;
+
+  for (let i = 0; i < galaxies.length; i++) {
+    for (let j = i + 1; j < galaxies.length; j++) {
+      pathSum += shortestPathOnGrid(galaxies[i], galaxies[j]);
+    }
+  }
+
+  return pathSum;
+}
+
+export default part1(input);
--- src/day11/part2.ts
+++ src/day11/part2.ts
@@ -1,5 +1,83 @@
 import { parseInput } from '../util';
+import { shortestPathOnGrid } from './part1';
 
-const input = parseInput();
+const input = parseInput({ split: { mapper: false } });
 
-// TODO: Complete Part 2
+/**
+ * --- Part Two ---
+ * The galaxies are much older (and thus much farther apart) than the researcher initially estimated.
+ *
+ * Now, instead of the expansion you did before, make each empty row or column one million times larger. That is, each empty row should be replaced with 1000000 empty rows, and each empty column should be replaced with 1000000 empty columns.
+ *
+ * (In the example above, if each empty row or column were merely 10 times larger, the sum of the shortest paths between every pair of galaxies would be 1030. If each empty row or column were merely 100 times larger, the sum of the shortest paths between every pair of galaxies would be 8410. However, your universe will need to expand far beyond these values.)
+ *
+ * Starting with the same initial image, expand the universe according to these new rules, then find the length of the shortest path between every pair of galaxies. What is the sum of these lengths?
+ */
+
+function part2(input: string[]): number {
+  const grid = input.map(line => line.split(''));
+
+  const galaxies: number[][] = [];
+
+  // Record positions of all #
+  for (let i = 0; i < grid[0].length; i++) {
+    for (let j = 0; j < grid.length; j++) {
+      if (grid[j][i] === '#') {
+        galaxies.push([i, j]);
+      }
+    }
+  }
+
+  // Look for empty rows
+  for (let y = grid.length - 1; y >= 0; y--) {
+    let allDots = true;
+    for (let x = 0; x < grid[0].length; x++) {
+      if (grid[y][x] !== '.') {
+        allDots = false;
+        break;
+      }
+    }
+
+    if (allDots) {
+      // Look for all galaxies with a y value greater than y and add 1000000 to their y value
+      for (let g = 0; g < galaxies.length; g++) {
+        if (galaxies[g][1] > y) {
+          galaxies[g][1] += 1000000 - 1;
+        }
+      }
+    }
+  }
+
+  // Look for empty columns
+  for (let x = grid[0].length - 1; x >= 0; x--) {
+    let allDots = true;
+    for (let y = 0; y < grid.length; y++) {
+      if (grid[y][x] !== '.') {
+        allDots = false;
+        break;
+      }
+    }
+
+    if (allDots) {
+      // Look for all galaxies with an x value greater than x and add 1000000 to their x value
+      for (const galaxy of galaxies) {
+        if (galaxy[0] > x) {
+          galaxy[0] += 1000000 - 1;
+        }
+      }
+    }
+  }
+
+  let pathSum = 0;
+
+  // For every pair
+  for (let i = 0; i < galaxies.length; i++) {
+    for (let j = i + 1; j < galaxies.length; j++) {
+      pathSum += shortestPathOnGrid(galaxies[i], galaxies[j]);
+    }
+  }
+
+  return pathSum;
+}
+
+export default part2(input);
